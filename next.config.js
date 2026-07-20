34841a1ded4bcd056b52d30d040bab000e54a5a1
priyanshurai10 <priyanshurai1211111@gmail.com>
2026-07-20 18:13:51 +0530

    Fix backend URL

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
   async rewrites() {
-    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://127.0.0.1:8000';
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'https://nyaya-ai-production-04ba.up.railway.app ';
     return [
       {
         source: '/api/:path*',
